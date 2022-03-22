67f41af0d656e57da763a2c6631bf9df52259b81
charles.simon-meunier <charles.simon-meunier@epita.fr>
2022-03-23 00:32:37 +0100

    Hero & Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
+import Home from "./pages/Home.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
@@ -10,7 +9,7 @@ function App() {
       <AnimatePresence exitBeforeEnter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<Home />} />
         </Routes>
       </AnimatePresence>
     </BrowserRouter>
