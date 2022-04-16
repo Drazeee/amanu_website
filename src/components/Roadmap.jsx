3b0d1736ae63e9577fd8b34603e0d8076e81a67a
gianluca.minoprio <gianluca.minoprio@epita.fr>
2022-04-16 16:04:46 +0200

    update phase roadmap

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -13,7 +13,7 @@ export default function Roadmap() {
       </h2>
       <div className="items">
         <div className="item">
-          <h3>Link the Physical Art with the Meta Art</h3>
+          <h3>Phase 1: Link the Physical Art with the Meta Art</h3>
           <div className="dot"></div>
           <p>
             Our first objective is to develop our main technology that enable users to buy a NFT in our Virtual Galeries and receive the same version in Physical Art.
@@ -26,21 +26,21 @@ export default function Roadmap() {
           </p>
         </div>
         <div className="item">
-          <h3> Devellopping our 3D scanning methods</h3>
+          <h3>Phase 2: Devellopping our 3D scanning methods</h3>
           <div className="dot"></div>
           <p>
             Our ambition is that our NFT get always closer to the real artwork we scanned. So our clients gets the most loyal experience the artist wanted to give in reality. To improve that fidelity will be improving step by step our scanner to be able to scan all types of artwork you could imagine. Going from a simple painting to a detailed sculpture.  
           </p>
         </div>
         <div className="item">
-          <h3>Implementation of fashion-related projects</h3>
+          <h3>Phase 3: Implementation of fashion-related projects</h3>
           <div className="dot"></div>
           <p>
             For us fashion is one of the most important form of art here in France. And we deeply believe that Digital wearable will be the next big thing in the NFT community. In out ambition to replicate artwork, we will be aming to work with Fashion brand to replicate or create clothes to the metaverse. Simulating faithfully the material and textile of our clothes. Working with us you will have the most realistic experience of wearing clothes in the Metaverse.
           </p>
         </div>
         <div className="item">
-          <h3>Provide a metaverse that best suit to our community</h3>
+          <h3>Phase 4: Provide a metaverse that best suit to our community</h3>
           <div className="dot"></div>
           <p>
            During all this process, hard work and creations, we will in the future get lot of experiences with the most famous Metaverse.
